a7aca3fbdeab37376a661dd770f66051bbbbdd51
harishkannarao <harish.gtec@gmail.com>
2017-07-05 10:41:06 +0530

    Creating a sample of TestNg unit test with surefire plugin
    Automatically generates html report

---
+++ SurefireTestNgUnitTest/src/test/java/com/harishkannarao/SurefireSampleTestNgUnitTest.java
@@ -0,0 +1,17 @@
+package com.harishkannarao;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SurefireSampleTestNgUnitTest {
+    @Test
+    public void surefireSampleUnitTest1() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void surefireSampleUnitTest2() {
+        Assert.assertTrue(true);
+    }
+}
